f527109ce07df29381bda1f219cf62a9633207c2
Leandri Beavis <leandrikuyk@gmail.com>
2022-09-04 22:17:59 -0400

    added handle change function to update state and added character count

--- client/src/pages/DiscussionForm.js
+++ client/src/pages/DiscussionForm.js
@@ -1,25 +1,50 @@
 import React, { useState } from "react";
+import { useMutation } from '@apollo/client';
+import { ADD_COMMENT } from "../utils/mutations";
 
 const DiscussionForm = ({handleSubmit}) => {
 
     const [text, setText] = useState("");
+    const [characterCount, setCharacterCount] = useState(0);
+    const [addComment, { error }] = useMutation(ADD_COMMENT);
 
     const isTextAreaDisabled = text.length === 0;
 
+    // update state based on form input changes
+    const handleChange = (e) => {
+        if (e.target.value.length <= 280) {
+            setText(e.target.value);
+            setCharacterCount(e.target.value.length);
+        }
+    };
+
     const onSubmit = (e) => {
         e.preventDefault()
+
+        try {
+            addComment({
+                // variables: { commentBody, picId }, // Giving me Error - HELP
+            });
+        // resets form
         handleSubmit(text);
         setText("");
+        } catch (e) {
+            console.error(e);
+        }
     }
 
     return (
-        <form onSubmit={onSubmit}>
-            <textarea 
-                className="discussion-form-textarea" 
-                value={text} 
-                onChange={(e) => setText(e.target.value)}/>
-            <button className="discussion-form-button btn btn-primary mb-2" disabled={isTextAreaDisabled}>Comment</button>
-        </form>
+            <form onSubmit={onSubmit}>
+                <p className={`m-0 ${characterCount === 280 || error ? 'text-error' : ''}`}>
+                    {characterCount}/280
+                    {error && <span className="ml-2">Something went wrong...</span>}
+                </p>
+                <textarea 
+                    className="discussion-form-textarea" 
+                    value={text} 
+                    onChange={handleChange}/>
+                <button className="discussion-form-button btn btn-primary mb-2" disabled={isTextAreaDisabled}>Comment</button>
+            </form>
     )
 }
 
